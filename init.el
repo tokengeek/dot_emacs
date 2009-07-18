bc30127e330e9fdd40fec9708e6d27555dd99d88
Paul Thornthwaite <tokengeek@gmail.com>
2009-07-18 17:34:48 +0100

    init.el, color-theme and gitignore initially

---
+++ init.el
@@ -0,0 +1,8 @@
+
+(require 'carbon-font)
+(fixed-width-set-default-fontset
+ "-apple-inconsolata-medium-r-normal--14-*-*-*-*-*-iso10646-1")
+
+(require 'color-theme)
+(color-theme-initialize)
+(color-theme-retro-orange)
