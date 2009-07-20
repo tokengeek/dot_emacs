055505b7e313b2c8881ae7d5ec92f2bcca255d5c
Paul Thornthwaite <paul.thornthwaite@sanger.ac.uk>
2009-07-20 12:37:06 +0100

    Set default font to Inconsolata

---
+++ init.el
@@ -0,0 +1,6 @@
+
+;; Setting font
+;; http://oscarbonilla.com/2007/12/beautiful-emacs/
+(require 'carbon-font)
+(fixed-width-set-default-fontset
+       "-apple-inconsolata-medium-r-normal--14-*-*-*-*-*-iso10646-1")
